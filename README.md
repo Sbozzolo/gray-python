909c774b598e3ecaa3332c65f86fed3d297a2c8c
Gabriele Bozzola <sbozzolator@gmail.com>
2020-06-04 15:09:15 -0700

    Add skeleton and first function

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# gray-python
+# GRay (post-processing)
+
 Python tools for GRay2
+
+## Installation
+
+Recommended way:
+```sh
+pip3 install -e . --user
+``
+
+## Functions implemented:
+
+- Read data from ASCII file
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="GRay",
+    version="0.1",
+    author='Gabriele Bozzola',
+    author_email='gabrielebozzola@arizona.edu',
+    packages=['gray'],
+    description='Postprocessing of GRay data.',
+    install_requires=["numpy"],
+    license='LICENSE.txt',
+)
